74cb49d03c40de9b42ee2e35080f3a13bb604554
madhank1800 <madhankumar1800@gmail.com>
2022-12-28 15:22:46 +0530

    comments add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { Productlist } from "./CONTENTS/Productlist";
 //import './App.css';
-import { BrowserRouter, Routes ,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Productdetails } from "./CONTENTS/Productdetails";
 import Header from "./CONTENTS/Header";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
-        
+          {/*  */}
+          {/*  */}
           <Route path="/" exact element={<Productlist />}></Route>
-        <Route path="/product/:productId" exact element={<Productdetails/>}></Route>
+          <Route
+            path="/product/:productId"
+            exact
+            element={<Productdetails />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
